4f33b0be89a9dde77663c3b351a7ee411b2a6957
Jaime Torres Benavente <jaime.torres.benavente@apipana.es>
2024-10-14 10:40:35 +0200

    init commit

---
+++ src/main/java/com/torresj/nursing_sas_ope_info/NursingSasOpeInfoApplication.java
@@ -0,0 +1,13 @@
+package com.torresj.nursing_sas_ope_info;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NursingSasOpeInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NursingSasOpeInfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/torresj/nursing_sas_ope_info/NursingSasOpeInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.torresj.nursing_sas_ope_info;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NursingSasOpeInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
