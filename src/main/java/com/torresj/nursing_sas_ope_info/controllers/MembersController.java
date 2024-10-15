6ac1e1fcc68b2e19a1348b80784e7e20c7ccb77d
Jaime Torres Benavente <jaime.torres.benavente@apipana.es>
2024-10-15 19:14:58 +0200

    Actuators added

--- src/main/java/com/torresj/nursing_sas_ope_info/controllers/MembersController.java
+++ src/main/java/com/torresj/nursing_sas_ope_info/controllers/MembersController.java
@@ -1,6 +1,5 @@
 package com.torresj.nursing_sas_ope_info.controllers;
 
-import com.torresj.nursing_sas_ope_info.dtos.MemberDto;
 import com.torresj.nursing_sas_ope_info.dtos.MemberResponseDto;
 import com.torresj.nursing_sas_ope_info.services.MembersService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -13,7 +12,11 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.util.List;
@@ -65,7 +68,7 @@ public class MembersController {
     public ResponseEntity<MemberResponseDto> getChannel(@Parameter(description = "Member id") @PathVariable int id) throws IOException {
         log.info("Getting member {}", id);
         var member = membersService.getMember(id);
-        log.info("Member {} found: {}",id, member);
+        log.info("Member {} found: {}", id, member);
         return ResponseEntity.ok(member);
     }
 }
