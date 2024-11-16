e45bc90d1a372157893e1bdc4afdf0a2f4e59463
Jaime Torres Benavente <jaime.torres.benavente@apipana.es>
2024-11-16 14:15:07 +0100

    metrics added

--- src/main/java/com/torresj/nursing_sas_ope_info/monitor/NursesControllerAspect.java
+++ src/main/java/com/torresj/nursing_sas_ope_info/monitor/NursesControllerAspect.java
@@ -33,4 +33,114 @@ public class NursesControllerAspect {
                 .register(meterRegistry);
         counter.increment();
     }
+
+    @Before(
+            value = "execution(* com.torresj.nursing_sas_ope_info.services.NursesService.getBolsaCriticsNurses(..))"
+    )
+    public void updateBolsaCriticsEndpointCounter(){
+        Counter counter = Counter.builder("critics_calls")
+                .description("a number of requests to /critics endpoint")
+                .register(meterRegistry);
+        counter.increment();
+    }
+
+    @Before(
+            value = "execution(* com.torresj.nursing_sas_ope_info.services.NursesService.getBolsaDialysisNurses(..))"
+    )
+    public void updateBolsaDialysisEndpointCounter(){
+        Counter counter = Counter.builder("dialysis_calls")
+                .description("a number of requests to /dialysis endpoint")
+                .register(meterRegistry);
+        counter.increment();
+    }
+
+    @Before(
+            value = "execution(* com.torresj.nursing_sas_ope_info.services.NursesService.getBolsaFamilyNurses(..))"
+    )
+    public void updateBolsaFamilyEndpointCounter(){
+        Counter counter = Counter.builder("family_calls")
+                .description("a number of requests to /family endpoint")
+                .register(meterRegistry);
+        counter.increment();
+    }
+
+    @Before(
+            value = "execution(* com.torresj.nursing_sas_ope_info.services.NursesService.getBolsaGyneNurses(..))"
+    )
+    public void updateBolsaGyneEndpointCounter(){
+        Counter counter = Counter.builder("gyne_calls")
+                .description("a number of requests to /gyne endpoint")
+                .register(meterRegistry);
+        counter.increment();
+    }
+
+    @Before(
+            value = "execution(* com.torresj.nursing_sas_ope_info.services.NursesService.getBolsaMentalHealthNurses(..))"
+    )
+    public void updateBolsaMentalEndpointCounter(){
+        Counter counter = Counter.builder("mental_calls")
+                .description("a number of requests to /mental endpoint")
+                .register(meterRegistry);
+        counter.increment();
+    }
+
+    @Before(
+            value = "execution(* com.torresj.nursing_sas_ope_info.services.NursesService.getBolsaNeonatesNurses(..))"
+    )
+    public void updateBolsaNeonatesEndpointCounter(){
+        Counter counter = Counter.builder("neonates_calls")
+                .description("a number of requests to /neonates endpoint")
+                .register(meterRegistry);
+        counter.increment();
+    }
+
+    @Before(
+            value = "execution(* com.torresj.nursing_sas_ope_info.services.NursesService.getBolsaNuclearNurses(..))"
+    )
+    public void updateBolsaNuclearEndpointCounter(){
+        Counter counter = Counter.builder("nuclear_calls")
+                .description("a number of requests to /nuclear endpoint")
+                .register(meterRegistry);
+        counter.increment();
+    }
+
+    @Before(
+            value = "execution(* com.torresj.nursing_sas_ope_info.services.NursesService.getBolsaPediatricianNurses(..))"
+    )
+    public void updateBolsaPediatricianEndpointCounter(){
+        Counter counter = Counter.builder("pediatrician_calls")
+                .description("a number of requests to /pediatrician endpoint")
+                .register(meterRegistry);
+        counter.increment();
+    }
+
+    @Before(
+            value = "execution(* com.torresj.nursing_sas_ope_info.services.NursesService.getBolsaSpecificMentalHealthNurses(..))"
+    )
+    public void updateBolsaMentalSpecificEndpointCounter(){
+        Counter counter = Counter.builder("mental_specific_calls")
+                .description("a number of requests to /mental/specific endpoint")
+                .register(meterRegistry);
+        counter.increment();
+    }
+
+    @Before(
+            value = "execution(* com.torresj.nursing_sas_ope_info.services.NursesService.getBolsaSurgeryRoomNurses(..))"
+    )
+    public void updateBolsaSurgeryRoomEndpointCounter(){
+        Counter counter = Counter.builder("surgery_calls")
+                .description("a number of requests to /surgery endpoint")
+                .register(meterRegistry);
+        counter.increment();
+    }
+
+    @Before(
+            value = "execution(* com.torresj.nursing_sas_ope_info.services.NursesService.getBolsaWorkNurses(..))"
+    )
+    public void updateBolsaWorkEndpointCounter(){
+        Counter counter = Counter.builder("work_calls")
+                .description("a number of requests to /work endpoint")
+                .register(meterRegistry);
+        counter.increment();
+    }
 }
